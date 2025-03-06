73a5810eb5d4265a0f27efc836e5688815321d2a
eedo_y <doup2001@naver.com>
2025-03-07 00:58:30 +0900

    ✨feat : 계층형 카테고리 기능 구현 (2) - 누락

--- src/main/java/com/zip/community/platform/application/port/in/board/GetCategoryUseCase.java
+++ src/main/java/com/zip/community/platform/application/port/in/board/GetCategoryUseCase.java
@@ -5,10 +5,10 @@ import com.zip.community.platform.domain.board.Category;
 
 import java.util.List;
 
-public interface GetCategoryInfoUseCase {
+public interface GetCategoryUseCase {
 
     // 루트 카테고리들 조회
-    List<Category> getRootInfo();
+    List<Category> getRootCategory();
 
     // 카테고리 아이디로 조회
     Category getByCategoryId(Long categoryId);
