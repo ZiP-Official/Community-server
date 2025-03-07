b98dcf6b4b2284b557dbf31c2a8b94044393c0c7
eedo_y <doup2001@naver.com>
2025-03-07 22:41:38 +0900

    ✨feat : 목록에서 추천수 변화 기능 구현

--- src/main/java/com/zip/community/platform/application/port/in/board/GetBoardUseCase.java
+++ src/main/java/com/zip/community/platform/application/port/in/board/GetBoardUseCase.java
@@ -9,14 +9,15 @@ public interface GetBoardUseCase {
     // 게시물 상세 조회
     Board getBoardById(Long boardId);
 
-    // 특정 카테고리에 있는 게시글 조회하기 (댓글은 숫자)
-    Page<Board> getByCategoryId(Long categoryId, Pageable pageable);
-    
+    // 최신 게시물 목록 조회
+    Page<Board> getBoards(Pageable pageable);
+
     // 인기 게시물 목록 조회
     Page<Board> getBoardsView(Pageable pageable);
 
-    // 최신 게시물 목록 조회
-    Page<Board> getBoards(Pageable pageable);
+
+    // 특정 카테고리에 있는 게시글 조회하기 (댓글은 숫자)
+    Page<Board> getBoardsByCategoryId(Long categoryId, Pageable pageable);
 
 
 }
