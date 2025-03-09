074bc8e5c93eb52ca3a26513bd50b45bc1b11794
eedo_y <doup2001@naver.com>
2025-03-10 00:59:21 +0900

    ✨ feat : 댓글 기능 추가 기능 구현 - Mongo 기반의 Document 사용 - Mongo는 구현만 해두고, 사용은 JPA로 사용할 계획

--- src/main/java/com/zip/community/platform/adapter/out/CommentMongoPersistenceAdapter.java
+++ src/main/java/com/zip/community/platform/adapter/out/CommentMongoPersistenceAdapter.java
@@ -1,7 +1,61 @@
 package com.zip.community.platform.adapter.out;
 
-import org.springframework.stereotype.Component;
+import com.zip.community.platform.adapter.out.mongo.comment.CommentDocument;
+import com.zip.community.platform.adapter.out.mongo.comment.CommentMongoRepository;
+import com.zip.community.platform.application.port.out.comment.LoadCommentPort;
+import com.zip.community.platform.application.port.out.comment.RemoveCommentPort;
+import com.zip.community.platform.application.port.out.comment.SaveCommentPort;
+import com.zip.community.platform.domain.comment.Comment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-@Component
-public class CommentMongoPersistenceAdapter {
+import java.util.List;
+import java.util.Optional;
+
+//@Component
+@RequiredArgsConstructor
+public class CommentMongoPersistenceAdapter implements SaveCommentPort, LoadCommentPort, RemoveCommentPort {
+
+    private final CommentMongoRepository repository;
+
+    /// SaveCommentPort
+    @Override
+    public Comment saveComment(Comment comment) {
+        CommentDocument document = CommentDocument.from(comment);
+
+        return repository.save(document)
+                .toDomain();
+    }
+
+    @Override
+    public boolean getCheckedExistComment(String id) {
+        return repository.existsById(id);
+    }
+
+    /// LoadCommentPort
+    @Override
+    public Optional<Comment> loadCommentById(String id) {
+
+        return repository.findById(id)
+                .map(CommentDocument::toDomain);
+    }
+
+    @Override
+    public Page<Comment> loadCommentsByBoardId(Long boardId, Pageable pageable) {
+
+        return repository.findByBoardId(boardId, pageable)
+                .map(CommentDocument::toDomain);
+    }
+
+    @Override
+    public List<Comment> loadCommentsByCommentId(String parentId) {
+        return List.of();
+    }
+
+    /// RemoveCommentPort
+    @Override
+    public void removeComment(String id) {
+
+    }
 }
--- src/main/java/com/zip/community/platform/adapter/out/mongo/comment/CommentMongoRepository.java
+++ src/main/java/com/zip/community/platform/adapter/out/mongo/comment/CommentMongoRepository.java
@@ -1,4 +1,10 @@
 package com.zip.community.platform.adapter.out.mongo.comment;
 
-public interface CommentMongoRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface CommentMongoRepository extends MongoRepository<CommentDocument, String> {
+    Page<CommentDocument> findByBoardId(Long boardId, Pageable pageable);
 }
