bb9f4e7615072709738f3539f581f4a445748ff2
kimjinbeom <kjui4645@gmail.com>
2025-03-10 19:47:11 +0900

    커밋 파일 수정

---
+++ src/main/java/com/zip/community/platform/application/port/out/member/MemberPort.java
@@ -0,0 +1,18 @@
+package com.zip.community.platform.application.port.out.member;
+
+import com.zip.community.platform.domain.member.Member;
+
+public interface MemberPort {
+
+    Member saveMember(Member member);       // 회원가입
+
+    Member loginMember(String email);       // 로그인 회원정보 가져오기
+
+    Member chkEmail(String email);         // 회원가입 이메일 체크
+
+    boolean chkNickName(String nickName);   // 닉네임 중복체크
+
+    void chgNickName(Long id, String nickName);    // 닉네임 변경
+
+    void chgLocation(Long id, String location);     // 지역 변경
+}
